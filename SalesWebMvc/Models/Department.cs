7357f217aad74333b7860331d2d5343267d09edd
LuizEddie <contato.luiz1997@gmail.com>
2025-08-05 11:39:10 -0300

    feat: Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -4,5 +4,29 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+    
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller s)
+        {
+            Sellers.Add(s);
+        }
+
+        public void RemoveSeller(Seller s)
+        {
+            Sellers.Remove(s);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        }
     }
 }
